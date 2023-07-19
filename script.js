14c79386536eaf8fbadc9138fa94ea64e9f1c271
Laxmi Shrestha <shresthalaxmi0825@gmail.com>
2023-07-20 00:07:32 +0545

    Trial

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict:"
+
+console.log("Welcome");
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+
+console.log("Screen width: " + screenWidth);
+console.log("Screen height: " + screenHeight);
